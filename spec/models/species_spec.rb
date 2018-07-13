500ae3243339a0e022c90e9923ea772851d3a719
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-07-13 17:23:40 -0400

    Specify a Species model
    A record of the Species model should have the following fields [datatypes]:
      common_name [string]
      scientific_name [string]
    
    The Species model should validate for the presence and type of each field before creating a new record.

---
+++ spec/models/species_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Species, type: :model do
+  it "has a common name and a scientific name" do
+    red_oak = Species.create(
+      common_name: "Red Oak",
+      scientific_name: "Quercus rubra"
+    )
+
+    expect(red_oak).to be_valid
+    expect(red_oak.common_name).to eq("Red Oak")
+    expect(red_oak.scientific_name).to eq("Quercus rubra")
+  end
+
+  it "validates for the presence of a common name and a scientific name" do
+    no_common_name = Species.create(
+      scientific_name: "Quercus rubra"
+    )
+    expect(no_common_name).to be_invalid
+
+    no_scientific_name = Species.create(
+      common_name: "Red Oak"
+    )
+    expect(no_scientific_name).to be_invalid
+  end
+
+  it "validates that the common name and scientific name must be strings" do
+    invalid_common_name = Species.create(
+      common_name: 3,
+      scientific_name: "Quercus rubra"
+    )
+    expect(invalid_common_name).to be_invalid
+
+    invalid_scientific_name = Species.create(
+      common_name: "Red Oak",
+      scientific_name: 3
+    )
+    expect(invalid_scientific_name).to be_invalid
+  end
+end
