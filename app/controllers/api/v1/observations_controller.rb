bb3e89d2ac0e9f09f77ecc1d68d6fdb4cd88426d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-07-18 18:20:15 -0400

    Implement responding to a request for a single Observation
    When an HTTP client sends a GET request to the path '/api/v1/observations/:id', where :id is the primary key of an Observation record, the server's response has JSON in the body which contains that particular Observation record. The Observation in the JSON has the following fields:
      id
      species_id
      latitude
      longitude
      date

--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ObservationsController < ApplicationController
     observations = Observation.all
     render json: observations
   end
+
+  def show
+    observation = Observation.find(params[:id])
+    render json: observation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/observations', to: 'observations#index'
+      get '/observations/:id', to: 'observations#show'
     end
   end
 end
