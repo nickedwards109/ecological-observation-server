431b2fe116b1c11553133efe487606dba805b4e5
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-07-18 18:14:11 -0400

    Specify the response to a request for a single Observation
    When an HTTP client sends a GET request to the path '/api/v1/observations/:id', where :id is the primary key of an Observation record, the server's response should have JSON in the body which contains that particular Observation record. The Observation in the JSON should have the following fields:
      id
      species_id
      latitude
      longitude
      date

--- spec/requests/api/v1/observation_request_spec.rb
+++ spec/requests/api/v1/observation_request_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe 'observation requests' do
     expect(observation).to include('longitude')
     expect(observation).to include('date')
   end
+
+  it 'responds to a request for a single Observation' do
+    create(:observation, id: 1)
+
+    get '/api/v1/observations/1'
+
+    expect(response).to be_success
+
+    observation = JSON.parse(response.body)
+    expect(observation).to include('id')
+    expect(observation).to include('species_id')
+    expect(observation).to include('latitude')
+    expect(observation).to include('longitude')
+    expect(observation).to include('date')
+  end
 end
