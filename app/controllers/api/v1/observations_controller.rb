363bae633691d6df5eeb7add2ff78a60dc699a87
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-07-18 19:38:16 -0400

    Implement creating a new Observation record
    When an HTTP client sends a POST request to the path:
    
    /api/v1/observations?observation[latitude]=<latitude>&observation[longitude]=<longitude>&observation[date]=<date>&observation[species_id]=<species_id>
    
    with valid values substituted for <latitude>, <longitude>, <date>, and <species_id>,
    
    the server creates a new Observation record in the database and sends a 200 OK response.

--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -8,4 +8,13 @@ class Api::V1::ObservationsController < ApplicationController
     observation = Observation.find(params[:id])
     render json: observation
   end
+
+  def create
+    Observation.create(observation_params)
+    render status: 200
+  end
+
+  def observation_params
+    params.require(:observation).permit(:latitude, :longitude, :date, :species_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/observations', to: 'observations#index'
       get '/observations/:id', to: 'observations#show'
+      post '/observations', to: 'observations#create'
     end
   end
 end
