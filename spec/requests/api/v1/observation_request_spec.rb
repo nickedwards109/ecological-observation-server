007b65f54fc786fde20e1b8142f91a2472f64536
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-07-19 18:54:03 -0400

    Add more test cases
    This commit specifies that a request with an authorization header containing the correct authorization token but otherwise improperly structured should result in a 400 response. In this case, the improper structure involves a typo in the 'Bearer' component of the authorization header's value.
    
    This commit also adds all of the test cases to the POST request section that previously were only tested for GET requests. Specifically, POST and GET requests are now tested for:
      - an authorization header that is properly structured but has the wrong authorization token
      - an authorization header that has the correct authorization token but is improperly structured
      - no authorization header
    
    All of these cases are already green under the current implementation.

--- spec/requests/api/v1/observation_request_spec.rb
+++ spec/requests/api/v1/observation_request_spec.rb
@@ -22,19 +22,26 @@ RSpec.describe 'observation requests' do
     expect(observation).to include('date')
   end
 
-  it 'responds to an unauthorized request for all Observations with a 400' do
+  it 'responds to unauthorized requests for all Observations with a 400' do
     create_list(:observation, 3)
     ENV['AUTH_TOKEN'] = '08545a3bcc797767d52fbf5c3459ebe2ff0178e88588f9415bc4560e16221c45658dd269d17340a98a9ed1875950873c7e7c62b2a844bcc906edab62c9adddf0'
 
+    # Request with an authorization header that is properly structured but has the wrong authorization token
     get '/api/v1/observations',
       params: nil,
       headers: { Authorization: 'Bearer asdf' }
-
     expect(response).to have_http_status(400)
     expect(response.body).to be_blank
 
-    get '/api/v1/observations'
+    # Request with an authorization header that has the correct authorization token but is improperly structured
+    get '/api/v1/observations',
+      params: nil,
+      headers: { Authorization: 'Bearerr 08545a3bcc797767d52fbf5c3459ebe2ff0178e88588f9415bc4560e16221c45658dd269d17340a98a9ed1875950873c7e7c62b2a844bcc906edab62c9adddf0' }
+    expect(response).to have_http_status(400)
+    expect(response.body).to be_blank
 
+    # Request with no authorization header
+    get '/api/v1/observations'
     expect(response).to have_http_status(400)
     expect(response.body).to be_blank
   end
@@ -57,19 +64,26 @@ RSpec.describe 'observation requests' do
     expect(observation).to include('date')
   end
 
-  it 'responds to an unauthorized request for a single Observation with a 400' do
+  it 'responds to unauthorized requests for a single Observation with a 400' do
     create(:observation, id: 1)
     ENV['AUTH_TOKEN'] = '08545a3bcc797767d52fbf5c3459ebe2ff0178e88588f9415bc4560e16221c45658dd269d17340a98a9ed1875950873c7e7c62b2a844bcc906edab62c9adddf0'
 
+    # Request with an authorization header that is properly structured but has the wrong authorization token
     get '/api/v1/observations/1',
       params: nil,
       headers: { Authorization: 'Bearer asdf' }
-
     expect(response).to have_http_status(400)
     expect(response.body).to be_blank
 
-    get '/api/v1/observations/1'
+    # Request with an authorization header that has the correct authorization token but is improperly structured
+    get '/api/v1/observations/1',
+      params: nil,
+      headers: { Authorization: 'Bearerr 08545a3bcc797767d52fbf5c3459ebe2ff0178e88588f9415bc4560e16221c45658dd269d17340a98a9ed1875950873c7e7c62b2a844bcc906edab62c9adddf0' }
+    expect(response).to have_http_status(400)
+    expect(response.body).to be_blank
 
+    # Request with no authorization header
+    get '/api/v1/observations/1'
     expect(response).to have_http_status(400)
     expect(response.body).to be_blank
   end
@@ -104,6 +118,7 @@ RSpec.describe 'observation requests' do
 
     initial_observation_count = Observation.all.count
 
+    # Request with an authorization header that is properly structured but has the wrong authorization token
     post '/api/v1/observations',
       params: {
         observation: {
@@ -114,9 +129,36 @@ RSpec.describe 'observation requests' do
         }
       },
       headers: { Authorization: 'Bearer asdf' }
-
     expect(response).to have_http_status(400)
     expect(response.body).to be_blank
+
+    # Request with an authorization header that has the correct authorization token but is improperly structured
+    post '/api/v1/observations',
+      params: {
+        observation: {
+          latitude: 43.888470,
+          longitude: -72.151481,
+          date: '2018-01-13',
+          species_id: 1
+        }
+      },
+      headers: { Authorization: 'Bearerr 08545a3bcc797767d52fbf5c3459ebe2ff0178e88588f9415bc4560e16221c45658dd269d17340a98a9ed1875950873c7e7c62b2a844bcc906edab62c9adddf0' }
+    expect(response).to have_http_status(400)
+    expect(response.body).to be_blank
+
+    # Request with no authorization header
+    post '/api/v1/observations',
+      params: {
+        observation: {
+          latitude: 43.888470,
+          longitude: -72.151481,
+          date: '2018-01-13',
+          species_id: 1
+        }
+      }
+    expect(response).to have_http_status(400)
+    expect(response.body).to be_blank
+
 
     final_observation_count = Observation.all.count
     created_observations_count = final_observation_count - initial_observation_count
