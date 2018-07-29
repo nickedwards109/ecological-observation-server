68fe657a4f8fc778dec3dfa12024931aea2b1cee
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-07-29 16:29:09 -0400

    Implement an observation's species in the response
    This commit implements the server responding to a request for observations with not only the observations themselves, but also the species associated with those observations.
    
    Each observation belongs to a species. When an observation is requested, the common name and scientific name of the observation's species are sent in the response, in addition to the id, latitude, longitude, and date of the observation itself.
    
    Also, in order to keep a consistent data exchange format, every data tuple in the response is an object in a collection (even if only one tuple is requested and the collection has a length of 1). Specifically, if a single observation is requested, the response body is a collection containing a single entry for the requested observation and its species. If all observations are requested, the response body is a collection containing entries that each contain an observation and its species.

--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -1,11 +1,34 @@
 class Api::V1::ObservationsController < AuthenticationController
   def index
-    observations = Observation.all
+    observations = ActiveRecord::Base.connection.execute("
+      SELECT observations.id,
+             observations.latitude,
+             observations.longitude,
+             observations.date,
+             species.common_name AS species_common_name,
+             species.scientific_name AS species_scientific_name
+      FROM observations
+      INNER JOIN species ON observations.species_id = species.id;
+      ")
     render json: observations
   end
 
   def show
-    observation = Observation.find(params[:id])
+    observation_id = params[:id]
+    # Prevent SQL injection by ensuring that the id is actually an integer
+    sanitized_observation_id = Integer(observation_id)
+    render 404 if sanitized_observation_id.class != Fixnum
+    observation = ActiveRecord::Base.connection.execute("
+      SELECT observations.id,
+             observations.latitude,
+             observations.longitude,
+             observations.date,
+             species.common_name AS species_common_name,
+             species.scientific_name AS species_scientific_name
+      FROM observations
+      INNER JOIN species ON observations.species_id = species.id
+      WHERE observations.id = #{sanitized_observation_id}
+      ")
     render json: observation
   end
 
